29c669ae22539811a66b291bb24463f944d285c7
Stephen A Smith <ssmit@udel.edu>
2016-01-27 17:36:49 -0500

    Added Pickups
    Added Pickups for health and points
    Implemented a Point System
    Started Enemy prefab
    Polished Character movement and physics
    Added Enemy physics material

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour
+{
+
+    public int healthAmount;
+    public int rotateAngle;
+    public float rotateSpeed;
+    public float bobHeight;
+    public float bobSpeed;
+    public float timer;
+
+    void Start()
+    {
+        StartCoroutine(rockAnimation());
+    }
+    IEnumerator rockAnimation()
+    {
+        float currentAngle = 0;
+        float currentHeight = 0;
+        float startHeight = transform.position.y;
+        //bobHeight += currentHeight;
+        while (true)
+        {
+            currentAngle = Mathf.PingPong(Time.time * rotateSpeed, rotateAngle * 2);
+            currentHeight = Mathf.PingPong(Time.time * bobSpeed, bobHeight);
+            gameObject.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, currentAngle - rotateAngle);
+            gameObject.transform.position = new Vector3(transform.position.x, currentHeight + startHeight, transform.position.z);
+            yield return new WaitForSeconds(timer);
+        }
+        //yield return null;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerControl>().SendMessage("AddHealth", 10);
+            Destroy(gameObject);
+        }
+    }
+}
