34601852b229122ae9cbf911d72b7dd7a798bf9e
Stephen A Smith <ssmit@udel.edu>
2016-02-04 00:03:25 -0500

    Final Level 1 polish
    Put most of the final touches on level 1
    added in background
    added level fad to the level exit
    added adio to main and demo screens

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LevelExit : MonoBehaviour
 {
 
+    public bool fade;
+    public GameObject blackOverlay;
     public int newLevelIndex;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-            Application.LoadLevel(newLevelIndex);
+        if (!fade)
+        {
+            if (other.CompareTag("Player"))
+                Application.LoadLevel(newLevelIndex);
+        } else
+        {
+            StartCoroutine(SlowFade());
+        }
+    }
+    IEnumerator SlowFade()
+    {
+        blackOverlay.SetActive(true);
+        yield return StartCoroutine(ColorFade());
+        //yield return new WaitForSeconds(2.5f);
+        Application.LoadLevel(newLevelIndex);
+    }
+    IEnumerator ColorFade()
+    {
+        for (int i = 1; i <= 60; i++)
+        {
+            blackOverlay.GetComponent<Image>().color = Color.Lerp(Color.clear, Color.black, i / 40.0f);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
 }
